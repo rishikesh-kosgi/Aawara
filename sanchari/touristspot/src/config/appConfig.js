cfa757808469d0efb50e73aa3ec0a18f42514573
Harikrishnancse <harikrishnan12b2@gmail.com>
2026-03-29 00:48:52 +0530

    added google web kit

--- sanchari/touristspot/src/config/appConfig.js
+++ sanchari/touristspot/src/config/appConfig.js
@@ -1,7 +1,4 @@
-export const PRODUCTION_CONFIG_URLS = [
-  'https://raw.githubusercontent.com/rishikesh-kosgi/apiurl/main/baseurl.json',
-  'https://raw.githubusercontent.com/rishikesh-kosgi/apiurl/master/baseurl.json',
-];
+export const PRODUCTION_CONFIG_URLS = [];
 
 export const DEV_FALLBACK_BASE_URLS = [
   'http://127.0.0.1:5000',
@@ -9,6 +6,8 @@ export const DEV_FALLBACK_BASE_URLS = [
   'http://192.168.29.68:5000',
 ];
 
-export const RELEASE_FALLBACK_BASE_URLS = [];
+export const RELEASE_FALLBACK_BASE_URLS = [
+  'https://api.aawaraapp.com',
+];
 
 export const API_TIMEOUT_MS = 15000;
--- sanchari/touristspot/src/config/googleAuth.js
+++ sanchari/touristspot/src/config/googleAuth.js
@@ -1,1 +1,1 @@
-export const GOOGLE_WEB_CLIENT_ID = '811231004431-slsvhr6pca5a10odqtq3oshgs6v4hqmh.apps.googleusercontent.com';
+export const GOOGLE_WEB_CLIENT_ID = '811231004431-uf2eotlfulu15j9oj06ook64uahu7u69.apps.googleusercontent.com';
